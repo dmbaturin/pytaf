153c3e68cb462bfbb2b295a9fb35208b1fc8a7f3
Daniil Baturin <daniil@baturin.org>
2013-10-10 18:33:04 +0700

    Bump version to 1.0.0, let's pretend it's stable now.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='pytaf',
-      version='0.1.4',
+      version='1.0.0',
       description='TAF (Terminal Aerodrome Forecast) parser and decoder',
       url='http://github.com/dmbaturin/pytaf',
       author='Daniil Baturin',
@@ -12,7 +12,7 @@ setup(name='pytaf',
       packages=['pytaf'],
       zip_safe=True,
       classifiers = [
-                        "Development Status :: 3 - Alpha",
+                        "Development Status :: 5 - Production/Stable",
                         "License :: OSI Approved :: MIT License",
                         "Operating System :: OS Independent",
                     ]
