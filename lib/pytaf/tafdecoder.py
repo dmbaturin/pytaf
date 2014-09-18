54a801dfa0a24c83d904b6cb3a99178c1bae2503
Cayetano Benavent <cayetanobv@gmail.com>
2014-09-18 14:12:24 +0200

    Bug fixed: tilldate on TEMPO header string was fromdate

--- lib/pytaf/tafdecoder.py
+++ lib/pytaf/tafdecoder.py
@@ -77,7 +77,7 @@ class Decoder(object):
 
         from_str = "From %(from_hours)s:%(from_minutes)s on the %(from_date)s: "
         prob_str = "Probability %(probability)s%% of the following between %(from_hours)s:00 on the %(from_date)s and %(till_hours)s:00 on the %(till_date)s: "
-        tempo_str = "Temporarily between %(from_hours)s:00 on the %(from_date)s and %(till_hours)s:00 on the %(from_date)s: "
+        tempo_str = "Temporarily between %(from_hours)s:00 on the %(from_date)s and %(till_hours)s:00 on the %(till_date)s: "
         becmg_str = "Gradual change to the following between %(from_hours)s:00 on the %(from_date)s and %(till_hours)s:00 on the %(till_date)s: "
 
         if "type" in _header:
